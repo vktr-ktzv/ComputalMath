bde1cecb445ff3821ce1f3ccbc131bbda6034dc3
Viktor <kutuzov.vm@phystech.edu>
2023-09-19 22:56:13 +0300

    2nd task

--- 2nd_seminar/1st_Task.py
+++ 2nd_seminar/1st_Task.py
@@ -1,53 +1,49 @@
-import matplotlib as plt
 import numpy as np
-from sympy import diff, symbols, cos, sin
+import math 
+import matplotlib.pyplot as plt
 
-def comb(n, k) :
-    if k ==0:
-        return 1
-    
-    kk = 1
-    nn = n 
-    for i in range(1, k) :
-        nn *= n - i 
-        kk *= i + 1    
-    return nn // kk
-    
+def derivative (f, x, n, d):
+    sum = 0
 
-def dev(n):
-    ans = 0
-    h = 1e-5
-    for k in range (0, n+1):
-        print('comb',comb(n, k))
-        print('sin', sin(k*h))
-        print('k',(-1)**k, k)
+    for k in range (n + 1):
+        sum += math.comb(n, k) * f(x + k * d) * ((-1)**(k+1))
 
-        ans += comb(n, k)*sin(k*h)*((-1)**k)
-        
-    ans = ans/(h**n)
-    return ans
+    sum = sum / (d**n)
+    return sum
 
-def fact(n):
-    tmp = 1
-    while n > 1:
-        tmp *= n
-        n -= 1
-    return tmp
-        
+def acc (f, x, deltax, deltay):
+
+    n = 0
+    u = f(0)
+    while (abs(u - f(x)) >=  deltay):
+        n += 1
+        u += derivative(f, 0, n, deltax) * (x ** n) / (math.factorial(n))
+    return n
+
+def mistake_graph (f, x, deltax):
+    n = 0
+    u = f(0)
+
+    mist = []
+    iterations = 7
+    arg_list = np.arange (1, iterations + 1, 1)
+
+    for i in range (iterations):
+        n += 1
+        u += derivative (f, 0, n, deltax) * (x ** n) / (math.factorial(n))
+        print ("N =", n, "u =", u, "|u - u*| =", abs(u - f(x))) 
+        mist.append(abs(f(x) - u))
+
+    plt.plot(arg_list, mist)
+    plt.xlabel("N")
+    plt.ylabel("Mistake")
+
+    plt.show()
+
+#####################
+
+mistake_graph(np.sin, 0.5, 0.001)
+print(acc(np.sin, 0.5, 0.001, 0.001))
 
 
-def func(n):
-    ans = 0
-    for i in (0, n):
-        ans += dev(n)*(0.5**n)/fact(n) 
-    return ans 
-
-    
-
-du = 1e-3
-u = np.sin(0.5)
-eps = u
-n = 0
-
-print(dev(5))
 
